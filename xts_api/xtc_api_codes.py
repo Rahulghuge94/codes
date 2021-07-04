ca79f2332581e8bcdca140037bca7c699d9f2b22
Rahulghuge94 <1994ghuge@gmail.com>
2021-07-04 11:47:56 +0530

    Update xtc_api_codes.py

--- xts_api/xtc_api_codes.py
+++ xts_api/xtc_api_codes.py
@@ -11,3 +11,10 @@ def get_data(instrumentid,starttime,endtime,compressionValue,exchangesegment):
     data=[[data[i][0],float(data[i][1]),float(data[i][2]),float(data[i][3]),float(data[i][4]),float(data[i][5]),float(data[i][6])] for i in range(len(data))]
     df=pd.DataFrame(data,columns=['Datetime','Open','High','Low',"Close",'Volume','OI'])
     return df
+def get_future(symbol,expiry):
+    month={"01":'Jan',"02":'Feb',"03":'Mar',"04":'Apr',"05":'May',"06":'Jun',"07":'Jul',"08":'Aug',"09":'Sep',"10":'Oct',"11":'Nov',"12":'Dec'}
+    date=expiry[6:]
+    mon=month[expiry[4:6]]
+    year=expiry[:4]
+    symbol=symbol.upper()
+    return xt1.get_future_symbol(2,'FUTIDX',symbol,f'{date}{mon}{year}')['result'][0]['ExchangeInstrumentID']
